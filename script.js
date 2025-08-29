1697584fe1e373a1158fca3c75117a7a4575ebe9
Cas <140918847+Cas-Romero@users.noreply.github.com>
2025-08-29 16:05:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const fileInput = promptForm.querySelector("#file-input");
 const fileUploadWrapper = promptForm.querySelector(".file-upload-wrapper");
 const themeToggleBtn = document.querySelector("#theme-toggle-btn");
 // API Setup
-const API_KEY = "AIzaSyDz9pUbp8y3cemj3WxEM47CGW0h4T1dOt4";
+const API_KEY = "";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${API_KEY}`;
 let controller, typingInterval;
 const chatHistory = [];
